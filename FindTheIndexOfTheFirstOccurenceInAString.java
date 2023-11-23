902620860c66f53d847f9b5f44350b746255a50b
Hariprasd Murugesan <hariprasad.murugesan96@gmail.com>
2023-11-23 18:59:45 +0200

    secondedit

--- FindTheIndexOfTheFirstOccurenceInAString.java
+++ FindTheIndexOfTheFirstOccurenceInAString.java
@@ -2,22 +2,7 @@ package Dsa.leetcode;
 
 class FindTheIndexOfTheFirstOccurenceInAString {
     public static void main(String[] args) {
-        dad
-                da
 
-                da
-                        dad
-                        adad
-                                dada
-                                dadad
-                                        dad
-                                        daddad
-                                                dada
-                                                dadada
-                                                        dda
-                                                        ddaad
-                                                                ad
-                                                                dada
     }
     public static void main(String[] args) {
         String haystack = "sadbutsad";
