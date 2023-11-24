2bdffbd33dd1f51edc327e876c425bb568f4ef8a
Hariprasd Murugesan <hariprasad.murugesan96@gmail.com>
2023-11-24 15:01:14 +0200

    its Finished

--- FindTheIndexOfTheFirstOccurenceInAString.java
+++ FindTheIndexOfTheFirstOccurenceInAString.java
@@ -1,14 +1,26 @@
 package Dsa.leetcode;
 
 class FindTheIndexOfTheFirstOccurenceInAString {
-    public static void main(String[] args) {
-        nee vera mari da mapla
-
-
+    public static int strStr (String haystack, String needle) {
+        int hay = haystack.length();
+        int nee = needle.length();
+        if (hay < nee){
+            return -1;
+        }
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            int j=0;
+            while (j<needle.length() && haystack.charAt(i+j) == needle.charAt(j)){
+                j++;
+                if (j==needle.length()){
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
     public static void main(String[] args) {
-        String haystack = "sadbutsad";
-        String needle = "sad";
+        String haystack = "leetcode";
+        String needle = "leeto";
 
         int index = strStr(haystack, needle);
 
@@ -18,5 +30,6 @@ class FindTheIndexOfTheFirstOccurenceInAString {
             System.out.println("Needle found at index: " + index);
         }
     }
+
 
 }
