ebe52a5c1e63b70d963a10136b7aa3df06cfa938
Hariprasd Murugesan <hariprasad.murugesan96@gmail.com>
2023-11-28 19:16:50 +0200

    This image has two methods and solution Ugly Number series

--- UglyNumberI.java
+++ UglyNumberI.java
@@ -1,6 +1,29 @@
 package Dsa.leetcode;
 
 class UglyNumberI {
+
+
+    public boolean isUgly(int n) {
+        if ( n <= 0){
+            return false;
+        }
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+
+        while (n % 5 == 0) {
+            n /= 5;
+        }
+
+        // If the remaining number is 1, it's an ugly number
+        return n == 1;
+
+    }
+/*
     public int nthUglyNumber(int n) {
         int p2 = 0;
         int p3 = 0;
@@ -26,10 +49,16 @@ class UglyNumberI {
 
         return uglyNumber[n - 1] ;
     }
+*/
 
     public static void main(String[] args) {
+
         UglyNumberI uglyNumber = new UglyNumberI();
-        System.out.println(uglyNumber.nthUglyNumber(10));  // Output: 12
-        System.out.println(uglyNumber.nthUglyNumber(1));   // Output: 1
+        System.out.println(uglyNumber.isUgly(14));
+        System.out.println(uglyNumber.isUgly(6));
+
+        /*UglyNumberI uglyNumber = new UglyNumberI();
+        System.out.println(uglyNumber.nthUglyNumber(10));
+        System.out.println(uglyNumber.nthUglyNumber(1));   */
     }
 }
